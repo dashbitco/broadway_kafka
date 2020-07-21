235b46d702268d53e02f7c7dc7957841fdf86b6b
Fernando Farias <fernando@contaswap.com>
2020-07-20 22:23:52 -0300

    Bump brod version to 3.14.0

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule BroadwayKafka.MixProject do
   defp deps do
     [
       {:broadway, "~> 0.6.0"},
-      {:brod, "~> 3.9.2"},
+      {:brod, "~> 3.14.0"},
       {:ex_doc, ">= 0.19.0", only: :docs}
     ]
   end
