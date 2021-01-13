c652048750bdb82f3cd6e2b8023620ed64cc4a53
amacciola <54288198+amacciola@users.noreply.github.com>
2021-01-13 13:30:55 -0800

    Fix/check if pid is alive on assignments revoked (#44)

--- lib/producer.ex
+++ lib/producer.ex
@@ -412,7 +412,14 @@ defmodule BroadwayKafka.Producer do
 
   @impl :brod_group_member
   def assignments_revoked(producer_pid) do
-    GenStage.call(producer_pid, :drain_after_revoke, :infinity)
+    # If the producer_pid is no longer alive, it means the revoke
+    # is happening due to a shutdown, so ignore it.
+    pid = Process.whereis(producer_pid)
+
+    if !is_nil(pid) and Process.alive?(pid) do
+      GenStage.call(producer_pid, :drain_after_revoke, :infinity)
+    end
+
     :ok
   end
 
