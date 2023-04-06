9426ec84c6fc65c1ed36f653379d64b0f5f05817
Milad <milad@high-mobility.com>
2023-04-06 04:28:59 +0200

    implement dequeue

--- lib/broadway_kafka/producer/buffer.ex
+++ lib/broadway_kafka/producer/buffer.ex
@@ -2,8 +2,18 @@ defmodule BroadwayKafka.Producer.Buffer do
   @moduledoc false
 
   # Implemenet Ring buffer using :queue
-  # On enqueue 
-  # Each item in the queue has the full buffer for a given key
+  #
+  # Each item in the queue contains {key, list}
+  # which key is {generation_id, topic, partition} and list is
+  # the full messages to be read for this topic/partition.
+  # 
+  # When dequeuing messages, `dequeue` pops out an item in the queue
+  # takes `count` amount of messages for that {key, list} and put the rest back
+  # into the end of the queue.
+  #
+  # This approach ensures a more equitable consumption of all partitions.
+
+  alias Broadway.Message
 
   @type t :: :queue.queue()
   @type generation_id :: non_neg_integer
@@ -16,7 +26,7 @@ defmodule BroadwayKafka.Producer.Buffer do
     :queue.new()
   end
 
-  @spec enqueue_with_key(t, key, list :: [Broadway.Message.t()]) :: t
+  @spec enqueue_with_key(t, key, list :: [Message.t()]) :: t
   @doc false
   def enqueue_with_key(buffer, _key, []) do
     buffer
@@ -27,5 +37,62 @@ defmodule BroadwayKafka.Producer.Buffer do
   end
 
   @spec empty?(t) :: boolean
+  @doc false
   defdelegate empty?(buffer), to: :queue, as: :is_empty
+
+  @spec dequeue(t, count :: non_neg_integer) ::
+          {t, list({key, dequeued :: list(Message.t()), last_item :: Message.t()}),
+           dequeued_count :: non_neg_integer}
+  @doc false
+  def dequeue(buffer, count) when count > 0 do
+    {buffer, reversed_acc, count_credit_left} = do_dequeue_many(buffer, count, [])
+    {buffer, reversed_acc, count - count_credit_left}
+  end
+
+  defp do_dequeue_many(buffer, _count = 0, acc) do
+    {buffer, acc, 0}
+  end
+
+  defp do_dequeue_many(buffer, count, acc) do
+    case queue_out(buffer, count) do
+      {new_buffer, {_key, _items, _last_item, _len = 0}} ->
+        {new_buffer, acc, count}
+
+      {new_buffer, {key, items, last_item, len}} ->
+        new_count = count - len
+        ret = {key, items, last_item}
+        do_dequeue_many(new_buffer, new_count, [ret | acc])
+    end
+  end
+
+  @spec queue_out(t, count :: non_neg_integer) ::
+          {t, {key, list(Message.t()), Message.t(), len :: non_neg_integer}}
+  defp queue_out(buffer, count) do
+    case :queue.out(buffer) do
+      {{:value, {key, list}}, buffer} ->
+        {items, last_item, len, rest} = split(list, count)
+        buffer = enqueue_with_key(buffer, key, rest)
+        record = {key, items, last_item, len}
+        {buffer, record}
+
+      {:empty, buffer} ->
+        {buffer, {_key = nil, _items = [], _last_item = nil, _len = 0}}
+    end
+  end
+
+  def split(list, count) do
+    split_list(list, count, nil, 0, [])
+  end
+
+  def split_list(rest, 0, last_item, length_of_new_list, acc) do
+    {Enum.reverse(acc), last_item, length_of_new_list, rest}
+  end
+
+  def split_list([], _count, last_item, length_of_new_list, acc) do
+    {Enum.reverse(acc), last_item, length_of_new_list, []}
+  end
+
+  def split_list([head | tail], count, _last_item, length_of_new_list, acc) do
+    split_list(tail, count - 1, head, length_of_new_list + 1, [head | acc])
+  end
 end
--- test/producer/buffer_test.exs
+++ test/producer/buffer_test.exs
@@ -41,4 +41,72 @@ defmodule BroadwayKafka.Producer.BufferTest do
       assert {:value, {^key, ^messages}} = :queue.out(new_buffer) |> elem(0)
     end
   end
+
+  describe "dequeue/2" do
+    setup do
+      key01 = {1239, "topic", 1}
+
+      message_batch01 =
+        for i <- 1..3, do: %Broadway.Message{data: nil, acknowledger: nil, metadata: %{offset: i}}
+
+      key02 = {1239, "topic", 2}
+
+      message_batch02 =
+        for i <- 11..13,
+            do: %Broadway.Message{data: nil, acknowledger: nil, metadata: %{offset: i}}
+
+      buffer =
+        Buffer.new()
+        |> Buffer.enqueue_with_key(key01, message_batch01)
+        |> Buffer.enqueue_with_key(key02, message_batch02)
+
+      {:ok, buffer: buffer}
+    end
+
+    test "dequeues elements based on requested number", %{buffer: buffer} do
+      {new_buffer, dequeued_items, dequeued_count} = Buffer.dequeue(buffer, 2)
+
+      assert dequeued_count == 2
+
+      assert [
+               {{1239, "topic", 1},
+                [
+                  %Broadway.Message{metadata: %{offset: 1}},
+                  %Broadway.Message{metadata: %{offset: 2}}
+                ], %Broadway.Message{metadata: %{offset: 2}}}
+             ] = dequeued_items
+
+      refute new_buffer == buffer
+    end
+
+    test "after dequeues elements, on the next run should dequeues from another key", %{
+      buffer: buffer
+    } do
+      {new_buffer, dequeued_items, _dequeued_count} = Buffer.dequeue(buffer, 1)
+      popped_key01 = List.first(dequeued_items) |> elem(0)
+
+      {_new_buffer, dequeued_items, _dequeued_count} = Buffer.dequeue(new_buffer, 1)
+      popped_key02 = List.first(dequeued_items) |> elem(0)
+
+      refute popped_key01 == popped_key02
+    end
+
+    test "dequeues all elements if count is greater than the number of elements", %{
+      buffer: buffer
+    } do
+      {new_buffer, _dequeued_items, dequeued_count} = Buffer.dequeue(buffer, 10)
+      assert dequeued_count == 6
+
+      assert Buffer.empty?(new_buffer)
+    end
+
+    test "after dequeues all elements for a given key, should remove the key from the queue", %{
+      buffer: buffer
+    } do
+      assert {new_buffer, dequeued_items, 3} = Buffer.dequeue(buffer, 3)
+      assert length(dequeued_items) == 1
+      assert {{:value, {_, _}}, new_buffer} = :queue.out(new_buffer)
+      assert Buffer.empty?(new_buffer)
+    end
+  end
 end
